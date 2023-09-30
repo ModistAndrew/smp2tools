77510734fd7239e42c3561cbd10dcd203d51fa2e
ModistAndrew <zhujinxuan2005@163.com>
2023-09-30 11:06:27 +0800

    loot table

--- src/main/java/modist/smp2tools/LootTableConfig.java
+++ src/main/java/modist/smp2tools/LootTableConfig.java
@@ -1,21 +1,51 @@
 package modist.smp2tools;
 
-import net.minecraft.resources.ResourceLocation;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class LootTableConfig {
   private final FileConfiguration config;
+  private final LootTableSettings global;
+  private static final String PREFIX = "loot_tables";
 
-  public LootTableConfig(FileConfiguration config){
+  public LootTableConfig(FileConfiguration config) {
     this.config = config;
+    this.global = getSettings("global");
   }
 
-  public LootTableSettings getSettings(ResourceLocation lootTable) {
-    return config.get("loot")
+  public LootTableSettings getSettings(String lootTable) {
+    LootTableSettings settings = new LootTableSettings();
+    ConfigurationSection section = config.getConfigurationSection(getPath(lootTable));
+    settings.range = section.getInt("range", global != null ? global.range : 16);
+    settings.function = section.getString("function", global != null ? global.function : "%level");
+    settings.failInfo = section.getString("fail_info", global != null ? global.failInfo : "");
+    settings.successInfo = section.getString("success_info", global != null ? global.successInfo : "");
+    return settings;
+  }
+
+  public boolean contains(String lootTable) {
+    return config.contains(getPath(lootTable));
+  }
+
+  private String getPath(String lootTable) {
+    return PREFIX + '.' + lootTable;
   }
 
   public static class LootTableSettings {
     public int range;
-    public int
+    public String function;
+    public String failInfo;
+    public String successInfo;
+
+    public int getCount(int level) {
+      String expression = function.replace("%level%", String.valueOf(level));
+      try {
+        return (int) MathHelper.eval(expression);
+      } catch (Exception e) {
+        Smp2Tools.INSTANCE.getLogger().warning("Fail to eval function " + expression);
+        e.printStackTrace();
+      }
+      return 0;
+    }
   }
 }
--- src/main/java/modist/smp2tools/Smp2Tools.java
+++ src/main/java/modist/smp2tools/Smp2Tools.java
@@ -1,5 +1,11 @@
 package modist.smp2tools;
 
+import io.papermc.paper.event.block.BlockBreakBlockEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentBuilder;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
@@ -7,6 +13,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.border.WorldBorder;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
@@ -14,6 +21,9 @@ import org.bukkit.craftbukkit.v1_20_R1.CraftWorld;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.persistence.PersistentDataType;
@@ -21,9 +31,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
 @DefaultQualifier(NonNull.class)
 public final class Smp2Tools extends JavaPlugin implements Listener {
   private LootTableConfig lootTableConfig;
+  public static Smp2Tools INSTANCE;
+
   private NamespacedKey getKey(String key) {
     return new NamespacedKey(this, key);
   }
@@ -42,6 +62,7 @@ public final class Smp2Tools extends JavaPlugin implements Listener {
 
   @Override
   public void onEnable() {
+    INSTANCE = this;
     lootTableConfig = new LootTableConfig(this.getConfig());
     this.getServer().getPluginManager().registerEvents(this, this);
     info("Smp2Tools Plugin Enabled");
@@ -77,16 +98,23 @@ public final class Smp2Tools extends JavaPlugin implements Listener {
       BlockEntity be = cw.getHandle().getBlockEntity(getPos(location));
       if (be instanceof RandomizableContainerBlockEntity randomizableContainerBlockEntity) {
         ResourceLocation lootTable = randomizableContainerBlockEntity.lootTable;
-        if (lootTable != null) {
+        if (lootTable != null && lootTableConfig.contains(lootTable.toString())) {
+          LootTableConfig.LootTableSettings settings = lootTableConfig.getSettings(lootTable.toString());
           PdcUtil.map(e.getPlayer().getPersistentDataContainer(), getKey("loot_penalty"), PersistentDataType.TAG_CONTAINER,
             e.getPlayer().getPersistentDataContainer().getAdapterContext().newPersistentDataContainer(),
             pdc -> {
               PdcUtil.map(pdc, getKey(lootTable), LootPenalty.LOOT_PENALTY, new LootPenalty(),
                 lp -> {
-                  info("Player:%s, LootTable:%s, Count:%d, Level:%d", e.getPlayer().getName(), lootTable, lp.count, lp.level);
-                  if (!lp.update(x -> x * 2)) {
-                    teleport(cw, location, 32);
+                  if (!lp.update(settings::getCount)) {
+                    e.getPlayer().sendMessage(Component.text(settings.failInfo.replace("%count%", String.valueOf(lp.count))
+                      .replace("%level%", String.valueOf(lp.level))
+                      .replace("%loot_table%", lootTable.toString()), NamedTextColor.RED));
+                    teleport(cw, location, settings.range);
                     e.setCancelled(true);
+                  } else {
+                    e.getPlayer().sendMessage(Component.text(settings.successInfo.replace("%count%", String.valueOf(lp.count))
+                      .replace("%level%", String.valueOf(lp.level))
+                      .replace("%loot_table%", lootTable.toString()), NamedTextColor.GREEN));
                   }
                   return lp;
                 });
@@ -95,6 +123,19 @@ public final class Smp2Tools extends JavaPlugin implements Listener {
         }
       }
     }
+  }
+
+  @EventHandler
+  public void onExplosion(BlockExplodeEvent event) {
+    if(event.getBlock())
+
+  }
+
+  @EventHandler
+  public void onExplosion(EntityExplodeEvent event) {
+    event.blockList().stream().filter(b -> {
+
+    });
   }
 
   private void teleport(CraftWorld cw, Location loc, int range) {
